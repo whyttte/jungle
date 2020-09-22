1b7c8050d735db4af2dfcfb1e27fcb99e34e6e2a
whyttte <fogboruche@yahoo.com>
2020-09-22 21:35:41 +0000

    fixed bugs on admin-category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,13 +1,13 @@
 class Admin::CategoriesController < ApplicationController
 
-  # http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def index
-    @categories =Category.all
+    @categories = Category.all
   end
 
   def new
-    @product = Category.new
+    @category = Category.new
   end
 
   def create
