57851189a7b7f777da8577d7fa6913f2bcd4a5dc
whyttte <fogboruche@yahoo.com>
2020-09-22 19:21:22 +0000

    fixed problems on about us and logins

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with :name => ENV['USERNAME'], :password => ENV['PASSWORD']
+  http_basic_authenticate_with :name => ENV['USERNAME'].to_s, :password => ENV['PASSWORD'].to_s
   def show
   end
 end
